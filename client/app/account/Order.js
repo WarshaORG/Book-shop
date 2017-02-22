ba08555a04d5d10a198d7321f38db795705836d7
KAM9077 <kamalhajjar907@gmail.com>
2017-02-22 16:54:48 +0200

    (add)add controller

---
+++ client/app/account/Order.js
@@ -0,0 +1,36 @@
+angular.module('book.Order1' , [])
+
+
+.controller('OrderController', function ($scope , $window , $location , Order) {
+  $scope.order = {};
+
+
+  // if($location.path() === '/books/add'){
+  // if(!$window.localStorage.getItem("user.type")) {
+  //       $location.path('/');
+  //     } 
+  // }
+  // function add order just for admin 
+  $scope.addorder = function () {
+    Order.addorder($scope.order)
+    .then(function (order) {
+      $scope.order=order
+      console.log(order)
+        $location.path('/orders');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  // function show order for user and admin
+  $scope.showorder = function () {
+    Order.showorder($scope.order).then(function(data) {
+      console.log(data)
+    $scope.order = data;
+    // $scope.$apply()
+  });
+  }
+//$scope.showorder();
+  });
+
