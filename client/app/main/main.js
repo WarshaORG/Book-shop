08ecd8d393ef7340c496d83e2436b8fadf80562e
Dua'a Maloalain <maloalain.duaa94@hotmail.com>
2017-02-23 12:51:24 +0200

    (feat )try to add flag

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,7 +1,13 @@
 angular.module('book.main', [])
 
 .controller('MainController', function ($scope) {
-	
+	 
+
+$scope.loggedIN=false
+
+$scope.show=function () {
+	 $scope.loggedIN= !$scope.loggedIN
+}
 });
 
 
